ffd41a5cb15bb2b10be8152674f73b83993db3be
Eduardo Sena Cavalcante <eduardosena147@outlook.com>
2024-04-16 17:45:35 -0300

    feat: carga inicial de arquivos

---
+++ beecrowd/beecrowd/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using beecrowd.Solucoes;
+
+namespace beecrowd
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			/* Alterando a culture info para en-US por questões de ponto e vírgula */
+			CultureInfo.CurrentCulture = new CultureInfo("en-US", false);
+
+			Iniciante solIni = new Iniciante();
+
+			solIni.s1002();
+		}
+	}
+}
---
+++ beecrowd/beecrowd/Solucoes/Iniciante.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beecrowd.Solucoes
+{
+	public class Iniciante
+	{
+		public void s1000()
+		{
+			Console.WriteLine("Hello World!");
+		}
+
+		public void s1001()
+		{
+			int A = Convert.ToInt32(Console.ReadLine());
+			int B = Convert.ToInt32(Console.ReadLine());
+			int X = A + B;
+			Console.WriteLine("X = " + X.ToString());
+		}
+
+		public void s1002()
+		{
+			double n = 3.14159;
+			double raio = Convert.ToDouble(Console.ReadLine());
+			double area = n * (raio * raio);
+			Console.WriteLine("A=" + area.ToString("0.0000"));
+		}
+	}
+}
